34fbdbdf09df131637b00aa012b8f87bc22aead7
christian-bromann <mail@christian-bromann.com>
2018-09-24 14:10:52 +0200

    make timeToFirstInteractive check optional

--- tests/scriptBlocking.e2e.js
+++ tests/scriptBlocking.e2e.js
@@ -17,14 +17,25 @@ describe('scriptBlocking', () => {
         browser.url('/')
         browser.endTracing()
     })
+    
+    it('should have something rendered within 2 seconds', () => {
+        const { firstMeaningfulPaint } = browser.getPerformanceMetrics()
+        expect(firstMeaningfulPaint).to.be.below(2 * 1000) // 2 seconds
+    })
 
-    it('should be interactible within 3 seconds', () => {
+    it('should be interactible within 3 seconds', function () {
         const { timeToFirstInteractive } = browser.getPerformanceMetrics()
-        expect(timeToFirstInteractive).to.be.below(3 * 1000)
+        
+        if (!timeToFirstInteractive) {
+            return this.skip()
+        }
+        
+        expect(timeToFirstInteractive).to.be.below(3 * 1000) // 3 seconds
     })
     
     it('should be loaded within 6 seconds', () => {
-        const { timeToFirstInteractive } = browser.getPerformanceMetrics()
-        expect(timeToFirstInteractive).to.be.below(6 * 1000)
+        const { load, firstMeaningfulPaint } = browser.getPerformanceMetrics()
+        expect(load).to.be.below(6 * 1000) // 6 seconds
+        expect(firstMeaningfulPaint).to.be.below(2 * 1000) // 2 seconds
     })
 })
