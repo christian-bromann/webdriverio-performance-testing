77c9891e45c81f4d0075474e3e9465d52c1e208a
christian-bromann <mail@christian-bromann.com>
2018-11-22 15:22:56 +0100

    updated example to work with latest WebdriverIO and Chrome

--- tests/scriptBlocking.e2e.js
+++ tests/scriptBlocking.e2e.js
@@ -18,30 +18,20 @@ describe('scriptBlocking', () => {
         browser.endTracing()
     })
     
-    it('should have something rendered within 3.5 seconds', () => {
+    it('should have something rendered within 3 seconds', () => {
         const { firstMeaningfulPaint } = browser.getPerformanceMetrics()
-        expect(firstMeaningfulPaint).to.be.below(3.5 * 1000) // 2 seconds
+        expect(firstMeaningfulPaint).to.be.below(3 * 1000) // 3 seconds
     })
 
     it('should be interactible within 3 seconds', function () {
         const { timeToFirstInteractive } = browser.getPerformanceMetrics()
-        
-        /**
-         * In most cases this metric can't be determined based on the nature of
-         * how it is measured. It would require to trace a time frame where the
-         * browser is not running any long tasks or is doing heavy I/O work. Since
-         * we stop tracing right after page load that window is often not captured.
-         */
-        if (!timeToFirstInteractive) {
-            return this.skip()
-        }
-        
         expect(timeToFirstInteractive).to.be.below(3 * 1000) // 3 seconds
     })
     
     it('should not increase SpeedIndex limit', () => {
-        const { perceptualSpeedIndex } = browser.getSpeedIndex()
-        expect(perceptualSpeedIndex).to.be.below(3.5 * 1000)
+        const { perceptualSpeedIndex, speedIndex } = browser.getSpeedIndex()
+        console.log({ perceptualSpeedIndex, speedIndex });
+        expect(perceptualSpeedIndex).to.be.below(4.2 * 1000)
     })
     
     it('should be loaded within 6.5 seconds', () => {
